e933f1eca8981afc6878b60a9d583cef37d2b3ba
Bayes Ahmed Shoharto <bayes@graduate.utm.my>
2022-01-23 20:43:37 +0800

    Update news model | controller | env-exp

--- api/news/controller.js
+++ api/news/controller.js
@@ -1,29 +1,30 @@
 const mongoose = require("mongoose");
 const News = require("./model");
 
-//Add New News Data Form
+//Add New News articles Form
 exports.addNewNews = async (req, res) => {
   const news = new News(req.body);
 
   try {
     const saveNews = await news.save();
-    res.status(201).json({ message: "Created!", data: saveNews });
+    res.status(201).json({ message: "Created!", articles: saveNews });
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
 };
 
-//Get Back All News Data
+//Get Back All News articles
 exports.getAllNews = async (req, res) => {
   try {
     const news = await News.find();
-    res.status(200).json({ message: "Fetched!", data: news });
+    const totalResults = await News.count()
+    res.status(200).json({ status: "ok", totalResults: totalResults, articles: news });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
 };
 
-//Get Single News Data By ObjectId
+//Get Single News articles By ObjectId
 exports.getNewsById = async (req, res) => {
   try {
     const { id: _id } = req.params;
@@ -33,30 +34,30 @@ exports.getNewsById = async (req, res) => {
     const news = await News.find({
       _id,
     });
-    res.status(200).json({ message: "Fetched!", data: news });
+    res.status(200).json({ message: "Fetched!", articles: news });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
 };
 
-//Delete Single News Data By ObjectId
+//Delete Single News articles By ObjectId
 exports.deleteNewsById = async (req, res) => {
   try {
     const { id: _id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(_id))
       return res.status(404).send("No news with that id");
-  
+
     await News.deleteOne({
       _id,
     });
-  
+
     res.status(200).json({ message: "Deleted!" });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
 };
 
-//Update Single News Data By ObjectId
+//Update Single News articles By ObjectId
 exports.updateNewsById = async (req, res) => {
   try {
     const { name, description, defaultValue } = req.body;
@@ -70,7 +71,7 @@ exports.updateNewsById = async (req, res) => {
       $set: req.body,
     });
     res.status(200);
-    res.status(200).json({ message: "Updated!", data: news });
+    res.status(200).json({ message: "Updated!", articles: news });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
--- api/news/model.js
+++ api/news/model.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 
 const NewsSchema = mongoose.Schema({
-  name: {
+  source: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  title: {
     type: String,
     required: true,
   },
@@ -9,11 +17,19 @@ const NewsSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  defaultValue: {
-    type: Number,
+  url: {
+    type: String,
     required: true,
   },
-  createdAt: {
+  urlToImage: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  publishedAt: {
     type: Date,
     default: Date.now,
   },
